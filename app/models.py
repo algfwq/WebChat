5bc5153342c3af77c455ab824082c2e159622699
algfwq <3104374883@qq.com>
2023-05-14 07:10:49 +0800

    修改数据库

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Settings(models.Model):
+    email_ture = models.CharField(max_length=5)
 
 class User(models.Model):
     username = models.CharField(max_length=15)
     password = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
-    order = models.CharField(max_length=100000000)
     super = models.CharField(max_length=10)
